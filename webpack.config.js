701b5170934ce40b5aead6661b2765fbca06cebe
Kevin Barabash <kevinb@khanacademy.org>
2017-03-08 21:22:52 -0500

    hello, world

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    context: path.join(__dirname, "lib"),
+    entry: "./index.js",
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "bundle.js",
+        // libraryTarget: "commonjs2"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+            }
+        ]
+    }
+}
